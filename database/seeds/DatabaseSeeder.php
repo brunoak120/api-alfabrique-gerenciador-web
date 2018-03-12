cf8ac66ace29e25dc92c8297dc9bd61b1a69e36b
brunoallison <bruno.allison@live.com>
2018-03-12 00:05:01 -0400

    Criação dos Seeds iniciais completo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(NivelAcessoTableSeeder::class);
+        $this->call(EstadoTableSeeder::class);
+        $this->call(CidadeTableSeeder::class);
+        $this->call(EnderecoTableSeeder::class);
         $this->call(UsuarioTableSeeder::class);
     }
 }
