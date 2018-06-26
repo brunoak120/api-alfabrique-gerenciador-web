44135b04346c18897ffc0cce370136d11c3ec090
bruno.allison <bruno.allison@live.com>
2018-06-25 23:20:34 -0400

    alterações nas caracteristicas

--- database/seeds/CaracteristicaSeeder.php
+++ database/seeds/CaracteristicaSeeder.php
@@ -16,12 +16,24 @@ class CaracteristicaSeeder extends Seeder
         DB::table('caracteristicas')->insert([
             [
                 'id' => 1,
+                'nome' => 'Sílaba',
+                'descricao' => 'Dificuldade na característica de quantidade de sílabas na palavra.',
+                "created_at" => $now,
+                "updated_at" => $now,
+            ], [
+                'id' => 2,
+                'nome' => 'Letras',
+                'descricao' => 'Dificuldade na característica de quantidade de letras na palavra.',
+                "created_at" => $now,
+                "updated_at" => $now,
+            ], [
+                'id' => 3,
                 'nome' => 'ç',
                 'descricao' => 'Palavras que possuem Ç',
                 "created_at" => $now,
                 "updated_at" => $now,
             ], [
-                'id' => 2,
+                'id' => 4,
                 'nome' => 'á',
                 'descricao' => 'Palavras que possuem acento agudo no a',
                 "created_at" => $now,
